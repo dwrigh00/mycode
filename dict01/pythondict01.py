cc4e5b09bd51f3705800dabbf6b8e3e4fb354777
Derith Wright <derithwright@users.noreply.github.com>
2022-01-25 17:59:32 +0000

    learning about dictionaries

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -6,7 +6,8 @@ switch = {"hostname": "sw1", "ip": "10.0.1.1", "version": "1.2", "vendor": "cisc
 ## display parts of the dictionary
 print( switch["hostname"] )
 print( switch["ip"] )
-  
+
+
 #!/usr/bin/env python3
 
 ## create a dictionary
