171c38c1f20c00f641d8bb08efe602a68acb25ce
Derith Wright <derithwright@users.noreply.github.com>
2022-01-25 17:20:05 +0000

    learning about dictionaries

---
+++ advlist/animal-list-test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main1():
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant" "Bee", "Cat", "Dog", "Tiger", "Wolf",]
+    
+def main2():
+    # create a list called list2
+    list2 = ["Bears", "Birds", "Dogs" "Lions", "Snakes",]
+
+
+    # list1
+    print(list1)
+
+    # list2
+    print(list2)
+
+main()
