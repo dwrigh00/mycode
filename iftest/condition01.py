d15f4cd553a1464d5d7791b889ca564b3e4166bb
Derith Wright <derithwright@users.noreply.github.com>
2022-01-25 21:33:28 +0000

    Learning about conditional if

--- iftest/condition01.py
+++ iftest/condition01.py
@@ -6,28 +6,4 @@ hostname = "MTG"
 # what to do if this statement is found to be true
 if hostname == "MTG":
     print("The hostname was found to be MTG")
-#!/usr/bin/env python3
-hostname = input("What value should we set for hostname?")
-if hostname == "MTG":
-    print("The hostname was found to be MTG")
-#!/usr/bin/env python3
-hostname = input("What value should we set for hostname?")
-## Notice how the next line has changed
-## here we use the str.lower() method to return a lowercase string
-if hostname.lower() == "mtg":
-    print("The hostname was found to be mtg")
-#!/usr/bin/env python3
-# Alta3 Research || Author: RZFeeser@alta3.com
-# Check hostname against expected value
-
-## Collect input from user
-hostname = input("What value should we set for hostname?")
-
-## use the lower method to test if input value matches expected value
-if hostname.lower() == "mtg":
-    print("The hostname was found to be mtg")
-    print("hostname matches expected config")
-
-## Always print out to the user
-print("Exiting the script")
 
