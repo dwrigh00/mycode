d2c366e6fd4876f02352df4105a14cd9b2ec27f9
Derith Wright <derithwright@users.noreply.github.com>
2022-01-25 16:07:16 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant" "Bee", "Cat", "Dog", "Tiger", "Wolf",]
+
+    # list1
+    print(list1)
+
+main()
