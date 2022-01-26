3b8cd283065664df89a645cca6a8c9fc6a59d5b7
Derith Wright <derithwright@users.noreply.github.com>
2022-01-26 15:40:58 +0000

    While looping

---
+++ custif/mymovies-picks.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   if, elif, else - A simple program using conditionals to make a decision."""
+
+
+
+message = 'The movie is about to begin, we recommend '
+
+# wrap connection in a float() to accept decimals as numbers
+connection = float(input("Fyi my favorite movie picks in sequential order?"))
+
+
+# if input value was higher or equal to 1
+if connection >= 01:
+    message = message + 'Hobbit & Lord of the Rings.'
+
+
+elif connection >= 02:
+    message = message + 'Heat.'
+elif connection >= 2:
+
+
+
+elif connection >= 03:
+    message = message + 'Gladiator.'
+elif connection >= 03:
+
+
+elif connection >= 04:
+    message = message + 'A Beautiful Mind.'
+elif connection >= 04:
+
